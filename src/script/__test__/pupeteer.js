48600697d8c21b8a8e6aefa534c78109035bfb6f
ElmiriYounes <elmiri.younes@hotmail.com>
2022-06-17 17:22:21 +0200

    r

---
+++ src/script/__test__/pupeteer.js
@@ -0,0 +1,26 @@
+const pupeteer = require("puppeteer");
+
+(async () => {
+  try {
+    const browser = await pupeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://www.takeaway.com/be');
+
+    // await page.$ equals to document.querySelector
+    // await page.$$ equals to document.querySelectorAll
+
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+})();
+
+// (async () => {
+//   await pupeteer
+//     .launch({ headless: false })
+//     .then(async (browser) => {
+//       const page = await browser.newPage();
+//       await page.goto("https://www.google.com/");
+//       console.log("its showing");
+//     })
+//     .catch((err) => console.error("Error: ", err));
+// })();
--- src/script/__test__/script.test.js
+++
@@ -1,8 +0,0 @@
-/**
- * @jest-environment jsdom
- */
-const sum = require('../sum');
-
-test('received string number and should add it as number', () => {
-    expect(sum('1', 3)).toBe(4);
-});
